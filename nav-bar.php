de8f528a5c3ae0308e8b35633bd7c550e938b73f
Nick Burdick <burd2790@gmail.com>
2021-02-22 18:16:57 -0500

    added basic nav structure

---
+++ nav-bar.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<header>
+    <section>
+      <div id="nav-bar">
+      <nav class="nav">
+        <a href="about-us.php">About Us</a>
+        <a href="news.php">News</a>
+        <a href="Contact.php">Contact</a>
+        <a href="archive.php">Archive</a>
+      </div>
+      </nav>
+    </section>
+  </header>
+
+
+</body>
+</html>
