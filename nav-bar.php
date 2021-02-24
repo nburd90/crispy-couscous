829829f17d39b884ac34cb5d273867f127fd5f1a
Nick Burdick <burd2790@gmail.com>
2021-02-23 19:34:44 -0500

    added home-page link

--- nav-bar.php
+++ nav-bar.php
@@ -10,21 +10,21 @@
 
 
 <header>
-    <section>
-      <div class="navbar">
-        <div class="dropdown">
-          <button class="dropbtn">Home
-            <i class="fa fa-caret-down"></i>
-          </button>
-          <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-          </div>
-        </div>
-      </div>
-    </section>
-  </header>
+		<section>
+			<div class="navbar">
+				<div class="dropdown">
+					<button class="dropbtn">Home
+						<i class="fa fa-caret-down"></i>
+					</button>
+					<div class="dropdown-content">
+						<a href="home-page.php">Home</a>
+						<a href="#">Link 2</a>
+						<a href="#">Link 3</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	</header>
 
 
 </body>
