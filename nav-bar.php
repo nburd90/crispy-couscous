bf16c7cdaddb9ac99dbeca5636466b56762c6b1e
Nick Burdick <burd2790@gmail.com>
2021-02-25 17:22:39 -0500

    removed unecessary code from nav header page

--- nav-bar.php
+++ nav-bar.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-<meta charset="utf-8">	
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
-
-<body>
-  
-  <header> 
+<header> 
   <nav class="navbar sticky-top navbar-dark bg-dark">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -47,5 +37,3 @@
   </header>
 
 
-</body>
-</html>
