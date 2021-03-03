43952f513efac493bf5b178dbf398c60a0e32830
Nick Burdick <burd2790@gmail.com>
2021-03-03 18:58:56 -0500

    deleted font links and added small blurb

--- about-us.php
+++ about-us.php
@@ -3,19 +3,36 @@
 
 <head>
         <title>About Us</title>
-        <link href="https://fonts.googleapis.com/css2?family=Reggae+One&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Padauk&family=Reggae+One&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&display=swap" rel="stylesheet">
+        <link href="" rel="stylesheet">
 </head>
 
 
 <?php include('nav-bar.php'); ?>
 
 <body>
-    
-        
+<div class="container">
+    <section>
+        <h1>About Us</h1>
+            <p>Behind the Reel specializes in the discovering and financing of independently led film projects. 
+                When creating the company we thought that people's passions should be centerstage. 
+                While branding ourselves as the wards of content creation, bringing Buffalo into the film-making forefront, 
+                and more importantly onto people's television screens became our mission.</p>
+            <p></p>
+            <p></p> 
+
+    </section>
 
 
 
+
+</div>
+
+
+
+
+
+
+
+<footer></footer>
 </body>
 </html>
