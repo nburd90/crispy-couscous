04e2fc1a95716341cdc64a09ff0487e29ae7c992
Nick Burdick <burd2790@gmail.com>
2021-03-04 12:24:40 -0500

    reformed page, added profile cards, and image carousel

--- about-us.php
+++ about-us.php
@@ -1,57 +1,94 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-        <title>About Us</title>
-        <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-</head>
-
-
-<?php include('nav-bar.php'); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Behind the Reel</title>
+  </head>
+  <?php include('nav-bar.php'); ?>
 
 <body>
-<div class="container">
-    <section>
-        <h1>About Us</h1>
-            <p>Behind the Reel specializes in the discovering and financing of independently led film projects. 
+    
+<main>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+    
+        <section>
+            <h1>About Us</h1>
+                <p>Behind the Reel specializes in the discovering and financing of independently led film projects. 
                 When creating the company we thought that people's passions should be centerstage. 
                 While branding ourselves as the wards of content creation, bringing Buffalo into the film-making forefront, 
                 and more importantly onto people's television screens became our mission.</p>
-            <b/><p></p>
-             
-
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <b/><p></p>
+        </section>
+   <!-- Wrap rest of page in container to center contet --> 
+        <div class="container marketing">
+            <div class="row">
+                <div class=".col-md-6">
+                    <img class="bd-placeholder-img rounded-circle" src="..." alt="Card image cap">
+                        <h2>heading</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                            Eligendi adipisci ut porro consectetur voluptate, pariatur autem sit, 
+                            explicabo sint at iste sunt voluptas, obcaecati modi. 
+                            Impedit necessitatibus incidunt ex laboriosam.
+                    <p>
+                        <a class="btn-btn-secondary" href="#" role="button"></a>
+                    </p>
+                </div>
+            
+                <div class="col-md-6">
+                    <img class="bd-placeholder-img rounded-circle" src="..." alt="Card image cap">
+                    <h2>heading</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Voluptates iste eaque, ipsum sapiente rem vel repellendus maxime accusamus excepturi 
+                        itaque qui porro quae facilis placeat, 
+                        dolores repudiandae neque libero natus. 
+                        card title and make up the bulk of the card's content.
+                    </p>
+                    <p>
+                        <a class="btn-btn-secondary" href="#" role="button"></a>
+                    </p>
+                </div>
+            </div>
         </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-
-    </section>
-
-
-
-
-</div>
+    
+<footer class="footer"></footer>        
+</main>
 
 
 
 
 
 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
 
-<footer></footer>
-</body>
+
+
+
 </html>
