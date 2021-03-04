e46b67edf8093c63e121ddb407e4e9cb7e7f63d8
Nick Burdick <burd2790@gmail.com>
2021-03-03 19:24:42 -0500

    added bootstrap stylesheet

--- about-us.php
+++ about-us.php
@@ -3,7 +3,8 @@
 
 <head>
         <title>About Us</title>
-        <link href="" rel="stylesheet">
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 
 
