f2efe3474758965a44b964c53f5da868fb011ad2
Nick Burdick <burd2790@gmail.com>
2021-03-04 19:59:17 -0500

    added img src within cards

--- about-us.php
+++ about-us.php
@@ -36,15 +36,17 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Special title treatment</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
