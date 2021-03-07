84a28fa0fdb40d2cb397d2548aa65edbbb56a283
Nick Burdick <burd2790@gmail.com>
2021-03-07 11:36:12 -0500

    fixed href syntax for bootstrap

--- about-us.php
+++ about-us.php
@@ -4,18 +4,17 @@
     
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>Behind the Reel</title>
   </head>
   
 
 <body>
+
 <?php include('nav-bar.php'); ?>
-    <div class="bannerImage">
-        <img src="crispy-images/indiefilms.jpg">
-    </div>
-    
-    <div class="container">  
+
+<div class="container">  
+<img src="crispy-images/indiefilms.jpg">
         <h1>About Us</h1>
         <div class="aboutUs">
             <p>When we created BehindTheReel we set out to empower people's passions and place them centerstage. 
@@ -52,19 +51,8 @@
                 </div>
             </div>
          </div>
-   
-   
    </div>
 
-
-<footer class="footer"></footer>        
-
-
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   </body>
-
-
-
-
 </html>
