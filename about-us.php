6cbc992a6f72ca130f267cc7e86bbb9ebff49e01
Nick Burdick <burd2790@gmail.com>
2021-03-08 09:41:40 -0500

    removed more white spaces b/w code

--- about-us.php
+++ about-us.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,8 +7,6 @@
     <link rel="stylesheet" href="styles.css">
     <title>Behind the Reel</title>
   </head>
-  
-
 <body>
     <header>
         <?php include('nav-bar.php'); ?> 
@@ -54,4 +51,4 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   </body>
-</html>
+  </html>
