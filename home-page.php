b25cc200d24414b8be9952ee0b062e45ba50006d
Nick Burdick <burd2790@gmail.com>
2021-03-07 22:19:39 -0500

    added custon styles for img tag - moved img src to header

--- home-page.php
+++ home-page.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" >
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css"/> 
     <title>Beyond the Reel</title>
-
-    <!--Will add an icon 
-    <link rel="shortcut icon" href="" type=""/> -->
 </head>
 
 
-<body>
+<body class="home-bg">
 <?php include('nav-bar.php'); ?>
-<div class="container"><!--will fill in--></div>
+
+<div class="containerHome"><!--will fill in--></div>
 
 
 
