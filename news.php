44d2e550c8e04fc4d6f75f1bdecb2d0fcb3b1786
Nick Burdick <burd2790@gmail.com>
2021-03-09 20:47:11 -0500

    further html formatting for #26

--- news.php
+++ news.php
@@ -8,92 +8,75 @@
   </head>
   <body>
   <?php include('nav-bar.php'); ?>
-    <h1>News</h1>
-        <div class="container">
-            <div class="newsPage">
-                <div class="newsSection">
-                    <!--<div width:23%; position: relative;>
+    <div class="container">
+        <div class="newsPage">
+            <h1>News</h1>
+            <div class="newsSection">
+                <!--<div width:23%; position: relative;>
                         <p class="header-medium">2021</p>
                     </div>
                     <div class="news-image-holder" style="background: url(crispy-images/newspic1.jpg);"></div>-->
-                    <div class="newsItem">
-                        <div class="newsItemSummary">
-                            <p class="header-news" style="font-size: 24px">
-                                Emma Corrin to Star Opposite Harry Styles 'My Policeman'
-                            <p class="news-date">09 February 2021</p>
-                            <p>
-                                "Coming off her Golden Globe and Sag Nominations for her
-                                portrayal of Princess Diana in&nbsp;
-                                <em>The Crown</em>
-                                &nbsp;will star opposite&nbsp;
-                                Harry Styles"
-                            </p>
-                        </div>
-                    
-                        <a href="https://www.independentfilmcompany.com/news/emma_corrin_to_star_opposite_harry_styles__my_policeman.html" target="blank">
-                            <button class="read-more-button">Read More</button>
-                        </a>
+                <div class="newsItem">
+                    <div class="newsItemSummary">
+                        <p class="header-news" style="font-size: 24px">
+                            Emma Corrin to Star Opposite Harry Styles 'My Policeman'
+                        <p class="news-date">09 February 2021</p>
+                        <p>
+                            "Coming off her Golden Globe and Sag Nominations for her
+                            portrayal of Princess Diana in&nbsp;
+                            <em>The Crown</em>
+                            &nbsp;will star opposite&nbsp;
+                            Harry Styles"
+                        </p>
                     </div>
-                </div>   
-            
+                    <a href="https://www.independentfilmcompany.com/news/emma_corrin_to_star_opposite_harry_styles__my_policeman.html" target="blank">
+                        <button class="read-more-button">Read More</button>
+                    </a>
+                </div>
+            </div>   
             <!-- Separate News Section-->
-            
-            <div class="newsItem">
-                <div width:23%; position: relative;>
-                    <p class="header-medium">2020</p>
+            <div class="newsSection">  
+                <!--<div width:23%; position: relative;>
+                <p class="header-medium">2020</p>
                 </div>
-            </div>
-            <div class="newsItem">
-                <div>
-                    <div class="news-image-holder" style="background:url(crispy-images/newspic2.jpeg)"></div>
-                </div>
-            <div class="td-relative">
-                <p class="header-news" style="font-size: 24px">
-                    Independent announces sales on documentary the "DONUT KING", 
-                    Led By A multi-territory Acquisition From VICE Media
-                <p class="news-date">30 November 2020</p>
-                <div class="newsItemSummary">
-                    <p>London, 30th Nov 2020 Independent has widely sold the SXSW winning documentary THE DONUT KING,
-                        with VICE acquiring the film in a multi-territory deal.
-                    </p>  
-                </p>              
-                </div>
-                <a href="https://www.independentfilmcompany.com/news/independent_announces_sales_on_documentary_the_donut_king__led_by_a_multiterritory_acquisition_from_vice_media.html"
+                <div class="news-image-holder" style="background:url(crispy-images/newspic2.jpeg)"></div>-->
+                <div class="newsItem">
+                    <div class="newsItemSummary">
+                        <p class="header-news" style="font-size: 24px">
+                            Independent announces sales on documentary the "DONUT KING", 
+                            Led By A multi-territory Acquisition From VICE Media
+                        <p class="news-date">30 November 2020</p>
+                        <p>London, 30th Nov 2020 Independent has widely sold the SXSW winning documentary THE DONUT KING,
+                            with VICE acquiring the film in a multi-territory deal.
+                        </p>  
+                    </div>
+                    <a href="https://www.independentfilmcompany.com/news/independent_announces_sales_on_documentary_the_donut_king__led_by_a_multiterritory_acquisition_from_vice_media.html"
                     target="blank">
-                    <button class="read-more-button">Read More</button>
-                </a>
+                        <button class="read-more-button">Read More</button>
+                    </a>
+                </div>
             </div>
-        </div>
-            
             <!-- Another News Section-->
-            <div class="newsItem">
-                <div width:23%; position: relative;>
+            <div class="newsSection">
+                <!--<div width:23%; position: relative;>
                     <p class="header-medium">2020</p>
                 </div>
-            </div>
-            <div class="newsItem">
-                <div>
-                    <div class="news-image-holder" style="background: url(crispy-images/newspic3.jpg);"></div>
-                </div>
-                <div class="td-relative">
-                    <p class="header-news" style="font-size: 24px">
-                        Vertigo Releasing Picks Up Cycling Drama 'The Racer' For UK & Ireland
-                    </p>
-                <p class="news-date">14 October 2020</p>
-                <div class="newsItemSummary">
-                    <p>
-                    Vertigo Releasing has acquired UK and Ireland distribution rights from Independent to Kieron J. Walshs cycling drama 
-                    <em>The Racer</em>
-                    </p>
-                </p>
-                </div>
-                <a href="https://www.independentfilmcompany.com/news/vertigo_releasing_picks_up_cycling_drama_the_racer_for_uk__ireland.html"
+                <div class="news-image-holder" style="background: url(crispy-images/newspic3.jpg);"></div>-->
+                <div class="newsItem">
+                    <div class="newsItemSummary">  
+                        <p class="header-news" style="font-size: 24px">
+                            Vertigo Releasing Picks Up Cycling Drama 'The Racer' For UK & Ireland
+                        </p>
+                        <p class="news-date">14 October 2020</p>
+                        <p>
+                        Vertigo Releasing has acquired UK and Ireland distribution rights from Independent to Kieron J. Walshs cycling drama 
+                        <em>The Racer</em>
+                        </p>
+                    </div>
+                    <a href="https://www.independentfilmcompany.com/news/vertigo_releasing_picks_up_cycling_drama_the_racer_for_uk__ireland.html"
                     target="blank">
                     <button class="read-more-button">Read More</button>
-                </a>
-                </div>
-                </div>
-                </div>
+                    </a>
                 </div>
             </div>
         </div>
