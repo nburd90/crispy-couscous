61baf227e53b9a8ab410dcf7b05d79560bf86d25
Nick Burdick <burd2790@gmail.com>
2021-03-11 09:24:12 -0500

    removed some whitespace and basicformatting

--- contact.php
+++ contact.php
@@ -27,86 +27,68 @@ if (!empty($process['message'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-    
-<head>
+  <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
-
 </head>
-
 <body>
-    
-
-<div class="container">
-  <div name="form"> 
-  
-    <form class="form-validation" roles="form" method="post" action="contact.php">
-      <div class="form-title-row">
-        <h1>Sign up for our movie pass newsletter!</h1>
-      </div>
-      <?php if ( ! empty ($errors)) : ?>
-        <div class="errors">
-          <p class="bg-danger"><?php echo implode('.</p><p class="bg-danger">', $errors); ?> .</p>
+  <div class="container">
+    <div name="form"> 
+      <form class="form-validation" roles="form" method="post" action="contact.php">
+        <div class="form-title-row">
+          <h1>Sign up for our movie pass newsletter!</h1>
         </div>
-      <?php elseif ( $sent ) : ?>
-        <div class="success">
-        <p class="bg-success">Your message was sent!</p>
-        </div>  
-      <?php endif; ?>
-      
-      <div class="form-row form-input-name-row">
-          <label>
-            <span>Your name</span>
+        <?php if ( ! empty ($errors)) : ?>
+          <div class="errors">
+            <p class="bg-danger"><?php echo implode('.</p><p class="bg-danger">', $errors); ?> .</p>
+          </div>
+        <?php elseif ( $sent ) : ?>
+          <div class="success">
+          <p class="bg-success">Your message was sent!</p>
+          </div>  
+        <?php endif; ?>
+        <div class="form-row form-input-name-row">
+            <label>
+              <span>Your name</span>
               <input id="name" type="text" name="name"  placeholder="Your Name Here" value="<?= validate_input('name');?> ">
-          </label>  
+            </label>  
+          </div>
+          <div class="form-row form-input-email-row">
+            <label>
+              <span>Email</span>  
+              <input id="email" type="email" name="email"  placeholder="Email" value="<?= validate_input('email');?> ">
+            </label>
+          </div>
+          <div class="form-row form-input-message-row">
+            <label>
+              <span>Your Message</span>
+              <textarea id="message" rows="4" name="message" ><?= validate_input('message');?></textarea>
+            </label> 
+          </div>
+          <div class="form-row form-input-human-row">
+            <label>
+              <span>2 + 3 = ?</span>
+              <input id="human" type="text" name="human" placeholder="Your Answer">
+            </label>
+          </div> 
+        <div class="form-group">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck">
+            <label class="form-check-label" for="gridCheck">
+              Click for personalized access!
+            </label>
+          </div>
         </div>
-        
-        <div class="form-row form-input-email-row">
-          <label>
-            <span>Email</span>  
-            <input id="email" type="email" name="email"  placeholder="Email" value="<?= validate_input('email');?> ">
-          </label>
+        <div class="form-row">
+          <button type="submit">Submit Form</button>
         </div>
-
-       <div class="form-row form-input-message-row">
-          <label>
-            <span>Your Message</span>
-            <textarea id="message" rows="4" name="message" ><?= validate_input('message');?></textarea>
-          </label> 
-        </div>
-
-        <div class="form-row form-input-human-row">
-          <label>
-            <span>2 + 3 = ?</span>
-            <input id="human" type="text" name="human" placeholder="Your Answer">
-          </label>
-        </div> 
-      
-      
-      <div class="form-group">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="gridCheck">
-          <label class="form-check-label" for="gridCheck">
-            Click for personalized access!
-          </label>
-        </div>
-      </div>
-      
-      <div class="form-row">
-        <button type="submit">Submit Form</button>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
-</div>
-    
-    
-
     <br>
     <br>
     <br>
-    
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
