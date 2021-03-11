f8d33197fbf73d0cd376d7591854021a1caa1fcd
Nick Burdick <burd2790@gmail.com>
2021-03-11 13:52:24 -0500

    formatted php

--- contact.php
+++ contact.php
@@ -26,7 +26,7 @@ if ( ! empty( $_POST)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
 </head>
 <body>
-  <?php if ( isset($insert)):?>
+  <?php if(isset($insert)):?>
     <div class="message">
     <?php if ($insert == true):?>
       <p class="success">This submission was succesful!</p>
