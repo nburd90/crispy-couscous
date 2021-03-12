430f5a9730856a1f42164238a4f98559d67d55ca
Nick Burdick <burd2790@gmail.com>
2021-03-12 12:22:47 -0500

    removed div w/ message class

--- contact.php
+++ contact.php
@@ -22,16 +22,7 @@ if ( ! empty( $_POST)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
 </head>
 <body>
-  
-    <div class="message">
-    
-      <p class="success">This submission was succesful!</p>
-      
-      <p class="error">There was an error with this submission.</p>
-      
-    </div>
-  
-<div class="container">
+  <div class="container">
     <div name="form"> 
       <?php 
       $newReader = new reader();
