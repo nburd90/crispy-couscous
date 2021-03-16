21578f35218fcf2bd54991c6c3ffddc71fd57962
Nick Burdick <burd2790@gmail.com>
2021-03-16 10:20:54 -0400

    placed span tags under inputs, debugged page for #17

--- contact.php
+++ contact.php
@@ -2,55 +2,59 @@
 ini_set('display_errors', 1);
 require_once('config.php');
 //Check for form submission - Is the form empty? 
-//if (!empty($_POST)) {
+if (!empty($_POST)) {
 	//Connect and escape
-	$mysql = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	//$mysql = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	//Submit to database
 	//$query = "INSERT INTO users (name,email) VALUES ('$name','$email')";
 	//$insert = $mysql->query($query);
-//}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
 </head>
+
 <body>
 	<div class="container">
 		<div name="form">
 			<?php
-			$name = "";
-			$email = "";
-			//$bError = false;
-			$error = array();
-			
+			//Define variables set to empty values
+			$name = $email = "";
+			$nameErr = $emailErr = "";
 
-			if (isset($_POST['submit-btn'])) {
-				if (!empty(trim($_POST["name"]))) {
-			} else {
-				//$bError = true;
-				$error['name'] = 'First name cannot be empty!';
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				//Check if name was submitted
+				if (empty($_POST["name"])) {
+					$nameErr = "Name is required.";
+				} else {
+					$name = $_POST['name'];
+				}
+				//Check if email is submitted
+				if (empty($_POST['email'])) {
+					$emailErr = "Email is required";
+				} else {
+					$email = $_POST['email'];
+				}
 			}
-
-			if (!empty(trim($_POST["email"]))) {
-			} else {
-				//$bError = true;
-				$error['email'] = 'Email cannot be empty!';
-			} 
 			?>
-			<form action="<?= htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+			<form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 				<div class="form-title-row">
 					<h1>Sign up for our movie pass newsletter!</h1>
 				</div>
 				<div class="form-field">
-					<label for="name">Your Name<span class="error">* <?php echo $nameErr;?></span></label>
-					<input type="text" name="name" placeholder="Your Name Here" value="<?php if(isset($_POST['name'])) echo htmlspecialchars($_POST['name']);?>">
+					<label for="name">Your Name</label>
+					<input type="text" name="name" placeholder="Your Name Here" value="<?php if (isset($_POST['name'])) echo htmlspecialchars($_POST['name']); ?>">
+					<span class="error">* <?php echo $nameErr ?></span>
 				</div>
 				<div class="form-field">
-					<label for="email">Email<span class="error">* <?php echo $emailErr;?></span></label>
-					<input type="email" name="email" placeholder="Email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
+					<label for="email">Email</label>
+					<input type="email" name="email" placeholder="Email" value="<?php if (isset($_POST['email'])) echo htmlspecialchars($_POST['email']); ?>">
+					<span class="error">* <?php echo $emailErr ?></span>
 				</div>
 				<div class="form-field">
 					<button type="submit">Sign Up</button>
@@ -60,4 +64,5 @@ require_once('config.php');
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
+
 </html>
