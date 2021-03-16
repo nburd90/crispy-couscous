52213427e07c4683ddb7012dfa155a6b8691823c
Nick Burdick <burd2790@gmail.com>
2021-03-16 12:37:32 -0400

    some php changes

--- contact.php
+++ contact.php
@@ -24,12 +24,15 @@ if (!empty($_POST)) {
 		<div name="form">
 			<?php
 			//Define variables set to empty values
-			$name = $email = "";
-			$nameErr = $emailErr = "";
+			$name = '';
+			$email = '';
+			$nameErr = '';
+			$emailErr = '';
 
-			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if (!empty($_POST['submit'])) {
+				var_dump($_POST);
 				//Check if name was submitted
-				if (empty($_POST["name"])) {
+				if (empty($_POST['name'])) {
 					$nameErr = "Name is required.";
 				} else {
 					$name = $_POST['name'];
@@ -42,24 +45,26 @@ if (!empty($_POST)) {
 				}
 			}
 			?>
-			<form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+			<form action="" method="post">
 				<div class="form-title-row">
 					<h1>Sign up for our movie pass newsletter!</h1>
 				</div>
 				<div class="form-field">
 					<label for="name">Your Name</label>
 					<input type="text" name="name" placeholder="Your Name Here" value="<?php if (isset($_POST['name'])) echo htmlspecialchars($_POST['name']); ?>">
-					<span class="error">* <?php echo $nameErr ?></span>
+					<span class="error">* <?= $nameErr ?? "" ?></span>
 				</div>
 				<div class="form-field">
 					<label for="email">Email</label>
 					<input type="email" name="email" placeholder="Email" value="<?php if (isset($_POST['email'])) echo htmlspecialchars($_POST['email']); ?>">
-					<span class="error">* <?php echo $emailErr ?></span>
+					<span class="error">* <?= $emailErr ?? "" ?></span>
 				</div>
 				<div class="form-field">
-					<button type="submit">Sign Up</button>
+					<button name="submit" type="submit">Sign Up</button>
 				</div>
 			</form>
+			<?= $name ?>
+			<?= $email ?>
 		</div>
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
